d1ff7302833803fef2541f2d5e3077904d5cfe84
Sven Brodny <svenbrodny@googlemail.com>
2022-07-10 01:42:21 +0200

    Added dark/light nodejs logo

--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Theme, ThemeService } from 'src/app/shared/theme.service';
+
 @Component({
   selector: 'section#skills',
   templateUrl: './skills.component.html'
 })
-export class SkillsComponent {}
+export class SkillsComponent {
+  public get nodeJsLogo(): string {
+    const color = this.theme.isDark ? Theme.LIGHT : Theme.DARK;
+    return `assets/skillset/nodejs-${color}.png`;
+  }
+  constructor(public theme: ThemeService) {}
+}
