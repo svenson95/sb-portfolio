167cf032e96ea31f1886e81a6d9ef151b70ca768
Sven Brodny <svenbrodny@googlemail.com>
2022-07-14 01:07:17 +0200

    Added header-button underline animation - and changed header-button styling mat-flat-button to mat-button (removed bg-color) - and set currently visible section while scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,43 @@
-import { Component } from '@angular/core';
+import { DOCUMENT, Location } from '@angular/common';
+import { Component, Inject, NgZone } from '@angular/core';
+import { debounceTime, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'sb-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  private scrollEvent$ = fromEvent(window, 'scroll').pipe(debounceTime(20));
+
+  constructor(private ngZone: NgZone, @Inject(DOCUMENT) private document: Document, private location: Location) {
+    // Inside runOutsideAngular callback, events which are usually triggering CD will not do it anymore - scroll event in this case
+    this.ngZone.runOutsideAngular(() => {
+      this.scrollEvent$.subscribe((event: Event) => {
+        // reenter the Angular zone
+        this.ngZone.run(() => {
+          // prevent set visible section when app starts
+          if (event.timeStamp > 1500) {
+            this.setVisibleSection();
+          }
+        });
+      });
+    });
+  }
+
+  private setVisibleSection(): void {
+    const sections = this.document.querySelectorAll('section');
+
+    Array.from(sections).some((section) => {
+      if (this.isInViewport(section)) {
+        this.location.replaceState('#' + section.id);
+        return true;
+      }
+      return false;
+    });
+  }
+
+  private isInViewport(el: HTMLElement): boolean {
+    const { top, bottom } = el.getBoundingClientRect();
+    return top <= 800 && bottom >= 300;
+  }
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { menu } from 'src/app/constants/menu';
@@ -8,4 +9,10 @@ import { menu } from 'src/app/constants/menu';
 })
 export class HeaderComponent {
   public menu = menu;
+
+  public sectionInViewport(menuItem: string): boolean {
+    return this.location.path(true).substring(1) === menuItem.substring(1);
+  }
+
+  constructor(private location: Location) {}
 }
