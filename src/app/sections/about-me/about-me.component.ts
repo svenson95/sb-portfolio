b3023963d9d483ba5f0eaebc6678e86c21847172
Sven Brodny <svenbrodny@googlemail.com>
2022-07-16 08:41:40 +0200

    Added about-me stagger-animation

--- src/app/sections/about-me/about-me.component.ts
+++ src/app/sections/about-me/about-me.component.ts
@@ -1,8 +1,29 @@
+import { trigger, transition, query, style, stagger, animate, keyframes } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'section#about',
-  templateUrl: './about-me.component.html'
+  templateUrl: './about-me.component.html',
+  animations: [
+    trigger('staggerAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(
+          ':enter',
+          stagger('100ms', [
+            animate(
+              '300ms ease-in',
+              keyframes([
+                style({ opacity: 0, transform: 'translateX(-20px)', offset: 0 }),
+                style({ opacity: 0.5, transform: 'translateX(10px)', offset: 0.3 }),
+                style({ opacity: 1, transform: 'translateX(0)', offset: 1 })
+              ])
+            )
+          ])
+        )
+      ])
+    ])
+  ]
 })
 export class AboutMeComponent {
   public data = [
