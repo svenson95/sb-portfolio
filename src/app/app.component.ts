8b6b3876f59856f4b1a6cda5e4d9d4fecf19a456
Sven Brodny <svenbrodny@googlemail.com>
2022-07-16 08:42:56 +0200

    refactor: scroll event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,13 @@
-import { DOCUMENT, Location } from '@angular/common';
-import { Component, Inject, NgZone } from '@angular/core';
-import { debounceTime, fromEvent } from 'rxjs';
+import { Component } from '@angular/core';
+
+import { ScrollService } from './shared/scroll.service';
 
 @Component({
   selector: 'sb-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  private scrollEvent$ = fromEvent(window, 'scroll').pipe(debounceTime(20));
-
-  constructor(private ngZone: NgZone, @Inject(DOCUMENT) private document: Document, private location: Location) {
-    // Inside runOutsideAngular callback, events which are usually triggering CD will not do it anymore - scroll event in this case
-    this.ngZone.runOutsideAngular(() => {
-      this.scrollEvent$.subscribe((event: Event) => {
-        // reenter the Angular zone
-        this.ngZone.run(() => {
-          // prevent set visible section when app starts
-          if (event.timeStamp > 1500) {
-            this.setVisibleSection();
-          }
-        });
-      });
-    });
-  }
-
-  private setVisibleSection(): void {
-    const sections = this.document.querySelectorAll('section');
-
-    Array.from(sections).some((section) => {
-      if (this.isInViewport(section)) {
-        this.location.replaceState('#' + section.id);
-        return true;
-      }
-      return false;
-    });
-  }
-
-  private isInViewport(el: HTMLElement): boolean {
-    const { top, bottom } = el.getBoundingClientRect();
-    return top <= 800 && bottom >= 300;
+  constructor(private scroll: ScrollService) {
+    this.scroll.initialize();
   }
 }
