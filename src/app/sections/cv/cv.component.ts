58b1760a796481d9d2ae138cc9cda73de6d9764e
Sven Brodny <svenbrodny@googlemail.com>
2022-07-22 02:51:41 +0200

    Updated cv & references order (newest first)

--- src/app/sections/cv/cv.component.ts
+++ src/app/sections/cv/cv.component.ts
@@ -4,4 +4,75 @@ import { Component } from '@angular/core';
   selector: 'section#cv',
   templateUrl: './cv.component.html'
 })
-export class CvComponent {}
+export class CvComponent {
+  public school = [
+    {
+      dateRange: '07.2010 - 10.2012',
+      value: 'Evangelische Schule Charlottenburg'
+    },
+    {
+      dateRange: '07.2008 - 06.2010',
+      value: 'Gottfried Kinkel Realschule'
+    },
+    {
+      dateRange: '07.2002 - 06.2008',
+      value: 'Christian Morgenstern Grundschule'
+    }
+  ];
+
+  public work = [
+    {
+      dateRange: '21.09.2021 - 21.02.2022',
+      value: 'Nebenbeschäftigung als Webentwickler <br /> <small>Kominal UG</small>'
+    },
+    {
+      dateRange: '01.09.2019 - 23.03.2022',
+      value: 'Ausbildung zum Fachinformatiker für Anwendungsentwicklung <br /> <small>Piobyte GmbH</small>'
+    },
+    {
+      dateRange: '01.03.2019 - 01.09.2019',
+      value: 'EQJ zum Fachinformatiker für Anwendungsentwicklung <br /> <small>Piobyte GmbH</small>'
+    },
+    {
+      dateRange: '18.06.2018 - 08.10.2018',
+      value: 'Produktionshelfer Handschriftautomation <br /> <small>Wunderpen GmbH</small>'
+    },
+    {
+      dateRange: '16.10.2017 - 31.12.2017',
+      value:
+        'Ausbildung zum Fliesen-, Platten- und Mosaikleger <br /> <small>Ulf Martischewski Fliesen, Naturstein, Spanndecken</small>'
+    },
+    {
+      dateRange: '01.09.2016 - 20.07.2017',
+      value: 'Ausbildung zum Fliesen-, Platten- und Mosaikleger <br /> <small>WH Werkhaus Berlin</small>'
+    },
+    {
+      dateRange: '01.10.2015 - 06.07.2016',
+      value: 'Ausbildung zum Fliesen-, Platten- und Mosaikleger <br /> <small>BfW Lehrbauhof Berlin</small>'
+    },
+    {
+      dateRange: '12.12.2014 - 18.01.2015',
+      value: 'Ausbildung zum Informatikkaufmann <br /> <small>ACDM (MAIL BOX - Zentrale)</small>'
+    },
+    {
+      dateRange: '01.08.2013 - 12.12.2014',
+      value: 'Ausbildung zur Fachkraft für Lagerlogistik <br /> <small>MAIL BOX - Direct-Marketing Service GmbH</small>'
+    },
+    {
+      dateRange: '10.05.2013 - 01.08.2013',
+      value: 'Lagerhelfer in Teilzeit <br /> <small>MAIL BOX - Direct-Marketing Service GmbH</small>'
+    },
+    {
+      dateRange: '29.04.2013 - 10.05.2013',
+      value: 'Praktikum als Fachkraft für Lagerlogistik <br /> <small>Party Rent Franchise GmbH</small>'
+    },
+    {
+      dateRange: '02.01.2013 - 22.04.2013',
+      value: 'EQJ zum Sport- und Fitnesskaufmann <br /> <small>EasyFitness Prenzlauer Berg</small>'
+    },
+    {
+      dateRange: '09.01.2012 - 27.01.2012',
+      value: 'Schulpraktikum beim Reparaturservice <br /> <small>Phonedoctor Berlin</small>'
+    }
+  ];
+}
--- src/app/sections/hobby-projects/hobby-projects.component.ts
+++ src/app/sections/hobby-projects/hobby-projects.component.ts
@@ -8,4 +8,46 @@ import { viewerOptions } from 'src/app/constants/viewer-options';
 })
 export class HobbyProjectsComponent {
   public options = viewerOptions;
+
+  public projects = [
+    {
+      title: 'Fachentwickler',
+      description: 'April 2020 - Heute',
+      content:
+        'Home-Schooling Plattform für die Berufsschule, Unterrichtszusammenfassungen, Quizze, Karteikarten & "Matching"-Puzzle-Minigame. Dashboard zum chronologischen abarbeiten aller Inhalte, Anzeige der letzten Ankündigung aus dem Schul-Newsarchiv, anstehender Klausuren, aktuellen Stundenplan und Lektionen der aktuellen Schulwoche.',
+      links: [
+        { href: null, label: 'GitHub' },
+        { href: 'https://github.com/svenson95/fachentwickler-app', label: 'Frontend' },
+        { href: 'https://github.com/svenson95/fachentwickler-api', label: 'Backend' }
+      ],
+      images: [
+        { src: 'assets/hobby-projects/fachentwickler-1.png', alt: 'Image 1' },
+        { src: 'assets/hobby-projects/fachentwickler-2.png', alt: 'Image 2' },
+        { src: 'assets/hobby-projects/fachentwickler-3.png', alt: 'Image 3' },
+        { src: 'assets/hobby-projects/fachentwickler-4.png', alt: 'Image 4' },
+        { src: 'assets/hobby-projects/fachentwickler-5.png', alt: 'Image 5' },
+        { src: 'assets/hobby-projects/fachentwickler-6.png', alt: 'Image 6' },
+        { src: 'assets/hobby-projects/fachentwickler-7.png', alt: 'Image 7' }
+      ]
+    },
+    {
+      title: 'ng-xmp<wbr />-animations',
+      description: 'September 2021 - Dezember 2021',
+      content:
+        "Tutorial App für geplanten schriftlichen Artikel mit Beispiel-Implementierungen von Angular's integrierten Animations-System. Animation bei State-Änderung, beim Erreichen eines Scrollpoints und <i>Stagger</i>-Animation.",
+      links: [
+        { href: 'https://github.com/svenson95/ng-xmp-animations', label: 'GitHub' },
+        { href: 'https://svenson95.github.io/ng-xmp-animations', label: 'Live-Preview' }
+      ]
+    },
+    {
+      title: 'Yahtzee Minigame',
+      description: 'September 2021',
+      content: 'Kleines Würfelspiel welches ich während einer Bewerbungsphase als Arbeitsprobe entwickeln sollte.',
+      links: [
+        { href: 'https://github.com/svenson95/yahtzee-app', label: 'GitHub' },
+        { href: 'https://codesandbox.io/s/github/svenson95/yahtzee-app', label: 'CodeSandbox' }
+      ]
+    }
+  ];
 }
