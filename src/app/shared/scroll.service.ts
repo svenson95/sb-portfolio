45dce5766aef6235ba61d0fee4841efc7b32b9c4
Sven Brodny <svenbrodny@googlemail.com>
2023-10-27 05:21:13 +0200

    Revert "remove unnecessary scroll service"
    This reverts commit 5197d163f606e15dbc1f0a1fefd3a9bb428b07fc.

---
+++ src/app/shared/scroll.service.ts
@@ -0,0 +1,42 @@
+import { DOCUMENT, Location } from '@angular/common';
+import { Inject, Injectable, NgZone } from '@angular/core';
+import { fromEvent, debounceTime } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScrollService {
+  private scrollEvent$ = fromEvent(window, 'scroll').pipe(debounceTime(20));
+
+  constructor(private ngZone: NgZone, @Inject(DOCUMENT) private document: Document, private location: Location) {}
+
+  public initialize(): void {
+    // Inside runOutsideAngular callback, events which are usually triggering CD will not do it anymore - scroll event in this case
+    this.ngZone.runOutsideAngular(() => {
+      this.scrollEvent$.subscribe((event: Event) => {
+        // reenter the Angular zone
+        this.ngZone.run(() => {
+          // prevent set visible section when app starts
+          this.setVisibleSection();
+        });
+      });
+    });
+  }
+
+  private setVisibleSection(): void {
+    const sections = this.document.querySelectorAll('section');
+
+    Array.from(sections).some((section) => {
+      if (this.isInViewport(section)) {
+        this.location.replaceState('#' + section.id);
+        return true;
+      }
+      return false;
+    });
+  }
+
+  private isInViewport(el: HTMLElement): boolean {
+    const { top, bottom } = el.getBoundingClientRect();
+    return top <= 800 && bottom >= 300;
+  }
+}
