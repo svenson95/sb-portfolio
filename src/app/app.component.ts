5197d163f606e15dbc1f0a1fefd3a9bb428b07fc
Sven Brodny <svenbrodny@googlemail.com>
2023-10-27 05:14:25 +0200

    remove unnecessary scroll service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,7 @@
-import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-
-import { ScrollService } from './shared/scroll.service';
 
 @Component({
   selector: 'sb-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  constructor(private location: Location, private scroll: ScrollService) {
-    this.scroll.initialize();
-  }
-}
+export class AppComponent {}
