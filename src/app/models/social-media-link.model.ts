614d6055a09cda7d2a0ca2feb954be0019ecd2d8
Sven Brodny <svenbrodny@googlemail.com>
2024-03-31 10:55:42 +0200

    fix(eslint): warnings in social media link model

--- src/app/models/social-media-link.model.ts
+++ src/app/models/social-media-link.model.ts
@@ -1,21 +1,22 @@
 export class SocialMediaLink {
-  title: string;
-  href: string;
+  public title: string;
+  public href: string;
 
-  constructor(props: {
-    title: string;
-    href: string;
-  }) {
+  constructor(props: { title: string; href: string }) {
     this.title = props.title;
     this.href = props.href;
   }
 
-  get iconSelector() {
+  public get iconSelector(): string {
     switch (this.title) {
-      case "linkedin": return "fa-brands fa-linkedin";
-      case "github": return "fa-brands fa-github";
-      case "mail": return "fa-solid fa-paper-plane";
-      default: throw new Error("unknown socials icon selector title")
+      case 'linkedin':
+        return 'fa-brands fa-linkedin';
+      case 'github':
+        return 'fa-brands fa-github';
+      case 'mail':
+        return 'fa-solid fa-paper-plane';
+      default:
+        throw new Error('unknown socials icon selector title');
     }
   }
 }
