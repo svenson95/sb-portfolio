cc4fd2c2c9148bc7c2b42236c95f43223279e44e
Sven Brodny <svenbrodny@googlemail.com>
2024-04-14 22:46:41 +0200

    style(cv): profession font-size & date format

--- src/app/sections/cv/cv.data.ts
+++ src/app/sections/cv/cv.data.ts
@@ -7,85 +7,85 @@ interface TableItem {
 
 export const CV_DATA: TableItem[] = [
   {
-    from: 'August 2022',
-    to: 'Oktober 2023',
+    from: 'Aug 2022',
+    to: 'Okt 2023',
     profession: 'Webentwickler - Frontend',
     company: 'Yolawo GmbH'
   },
   {
-    from: 'September 2021',
-    to: 'Februar 2022',
+    from: 'Sep 2021',
+    to: 'Feb 2022',
     profession: 'Nebenjob als Webentwickler - Frontend',
     company: 'Kominal UG'
   },
   {
-    from: 'September 2019',
-    to: 'März 2022',
+    from: 'Sep 2019',
+    to: 'Mär 2022',
     profession: 'Ausbildung zum Fachinformatiker für Anwendungsentwicklung',
     company: 'Piobyte GmbH'
   },
   {
-    from: 'März 2019',
-    to: 'September 2019',
+    from: 'Mär 2019',
+    to: 'Sep 2019',
     profession: 'EQJ als Webentwickler & App-Entwickler (Einstiegs<wbr/>qualifizierungs<wbr/>jahr)',
     company: 'Piobyte GmbH'
   },
   {
-    from: 'Juni 2018',
-    to: 'Oktober 2018',
+    from: 'Jun 2018',
+    to: 'Okt 2018',
     profession: 'Produktionshelfer bei einem StartUp (Handschriftautomation)',
     company: 'Wunderpen GmbH'
   },
   {
-    from: 'Oktober 2017',
-    to: 'Dezember 2017',
+    from: 'Okt 2017',
+    to: 'Dez 2017',
     profession: 'Ausbildung zum Fliesen-, Platten- und Mosaikleger',
     company: 'Ulf Martischewski Fliesen, Naturstein, Spanndecken'
   },
   {
-    from: 'September 2016',
-    to: 'Juli 2017',
+    from: 'Sep 2016',
+    to: 'Jul 2017',
     profession: 'Ausbildung zum Fliesen-, Platten- und Mosaikleger',
     company: 'WH Werkhaus Berlin'
   },
   {
-    from: 'Oktober 2015',
-    to: 'Juli 2016',
+    from: 'Okt 2015',
+    to: 'Jul 2016',
     profession: 'Ausbildung zum Fliesen-, Platten- und Mosaikleger',
     company: 'BfW Lehrbauhof Berlin'
   },
   {
-    from: 'Dezember 2014',
-    to: 'Januar 2015',
+    from: 'Dez 2014',
+    to: 'Jan 2015',
     profession: 'Ausbildung zum Informatikkaufmann',
     company: 'ACDM (MAIL BOX - Zentrale)'
   },
   {
-    from: 'August 2013',
-    to: 'Dezember 2014',
+    from: 'Aug 2013',
+    to: 'Dez 2014',
     profession: 'Ausbildung zur Fachkraft für Lagerlogistik',
     company: 'MAIL BOX - Direct-Marketing Service GmbH'
   },
   {
     from: 'Mai 2013',
-    to: 'August 2013',
+    to: 'Aug 2013',
     profession: 'Lagerhelfer in Teilzeit',
     company: 'MAIL BOX - Direct-Marketing Service GmbH'
   },
   {
-    from: 'April 2013',
+    from: 'Apr 2013',
     to: 'Mai 2013',
     profession: 'Praktikum als Fachkraft für Lagerlogistik',
     company: 'Party Rent Franchise GmbH'
   },
   {
-    from: 'Januar 2013',
-    to: 'April 2013',
+    from: 'Jan 2013',
+    to: 'Apr 2013',
     profession: 'EQJ als Sport- und Fitnesskaufmann (Einstiegs<wbr/>qualifizierungs<wbr/>jahr)',
     company: 'EasyFitness Prenzlauer Berg'
   },
   {
-    from: 'Januar 2012',
+    from: 'Jan 2012',
     to: null,
     profession: 'Schulpraktikum bei einem Smartphone-Reparaturservice',
     company: 'Phonedoctor Berlin'
