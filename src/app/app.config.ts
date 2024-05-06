013270c44c8f5cb4ce19eb05591a271031caea89
Sven Brodny <svenbrodny@googlemail.com>
2024-05-06 17:22:08 +0200

    refactor(config): storage providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
 import { APP_INITIALIZER, ApplicationConfig, StaticProvider, importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ScrollEventService } from './shared';
-import { STORAGE_PROVIDERS } from './shared/storage.service';
+import { LocalStorage, ScrollEventService, SessionStorage, WindowToken, getStorage, windowProvider } from './shared';
+
+export const STORAGE_PROVIDERS: StaticProvider[] = [
+  { provide: WindowToken, useFactory: windowProvider },
+  {
+    provide: LocalStorage,
+    useFactory: (win: Window) => getStorage(win, 'localStorage'),
+    deps: [WindowToken]
+  },
+  {
+    provide: SessionStorage,
+    useFactory: (win: Window) => getStorage(win, 'sessionStorage'),
+    deps: [WindowToken]
+  }
+];
 
 export const SCROLL_EVENT_PROVIDERS: StaticProvider[] = [
   { provide: APP_INITIALIZER, useFactory: () => () => null, deps: [ScrollEventService], multi: true }
--- src/app/shared/storage.service.ts
+++ src/app/shared/storage.service.ts
@@ -1,26 +1,10 @@
 /* eslint-disable no-empty-function */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
-import { InjectionToken, StaticProvider } from '@angular/core';
-
-import { WindowToken, windowProvider } from './window';
+import { InjectionToken } from '@angular/core';
 
 export const LocalStorage = new InjectionToken<Storage>('LocalStorage');
 export const SessionStorage = new InjectionToken<Storage>('SessionStorage');
-
-export const STORAGE_PROVIDERS: StaticProvider[] = [
-  { provide: WindowToken, useFactory: windowProvider },
-  {
-    provide: LocalStorage,
-    useFactory: (win: Window) => getStorage(win, 'localStorage'),
-    deps: [WindowToken]
-  },
-  {
-    provide: SessionStorage,
-    useFactory: (win: Window) => getStorage(win, 'sessionStorage'),
-    deps: [WindowToken]
-  }
-];
 
 export class NoopStorage implements Storage {
   length = 0;
@@ -35,9 +19,10 @@ export class NoopStorage implements Storage {
   setItem() {}
 }
 
-function getStorage(win: Window, storageType: 'localStorage' | 'sessionStorage'): Storage {
-  // When cookies are disabled in the browser, even trying to access `window[storageType]` throws an
-  // error. If so, return a no-op storage.
+type StorageType = 'localStorage' | 'sessionStorage';
+export function getStorage(win: Window, storageType: StorageType): Storage {
+  // When cookies are disabled in the browser, trying to access `window[storageType]` throws an error.
+  // This is a workaround to prevent the application from crashing in that scenario.
   try {
     return win[storageType];
   } catch {
