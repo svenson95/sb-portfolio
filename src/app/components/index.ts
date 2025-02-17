aa7908b3e7cbb5179f28868d4998b8777d4e7573
Sven Brodny <svenbrodny@googlemail.com>
2025-02-17 13:50:56 +0100

    refactor: remove unused link & in-new-tab comp / directive (2)

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,6 +1,4 @@
 export * from './card-header/card-header.component';
 export * from './footer/footer.component';
 export * from './header/header.component';
-export * from './link/in-new-tab.directive';
-export * from './link/link.component';
 export * from './projects-accordion/projects-accordion.component';
