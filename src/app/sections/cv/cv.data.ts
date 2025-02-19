1a9b8b8d39b3673dea2a9bb04034ac26a9a8aa5d
Sven Brodny <svenbrodny@googlemail.com>
2025-02-20 00:15:57 +0100

    fix(cv): invalid dates in safari

--- src/app/sections/cv/cv.data.ts
+++ src/app/sections/cv/cv.data.ts
@@ -7,85 +7,85 @@ interface TableItem {
 
 export const CV_DATA: TableItem[] = [
   {
-    from: new Date('2022/08'),
-    to: new Date('2023/10'),
+    from: new Date('2022/08/15'),
+    to: new Date('2023/10/15'),
     profession: 'web-developer',
     company: 'Yolawo GmbH'
   },
   {
-    from: new Date('2021/09'),
-    to: new Date('2022/02'),
+    from: new Date('2021/09/15'),
+    to: new Date('2022/02/15'),
     profession: 'part-time-job-web-dev',
     company: 'Kominal UG'
   },
   {
-    from: new Date('2019/09'),
-    to: new Date('2022/03'),
+    from: new Date('2019/09/15'),
+    to: new Date('2022/03/15'),
     profession: 'apprenticeship-it-app-dev',
     company: 'Piobyte GmbH'
   },
   {
-    from: new Date('2019/03'),
-    to: new Date('2019/09'),
+    from: new Date('2019/03/15'),
+    to: new Date('2019/09/15'),
     profession: 'internship-web-app-dev',
     company: 'Piobyte GmbH'
   },
   {
-    from: new Date('2018/06'),
-    to: new Date('2018/10'),
+    from: new Date('2018/06/15'),
+    to: new Date('2018/10/15'),
     profession: 'production-assistant',
     company: 'Wunderpen GmbH'
   },
   {
-    from: new Date('2017/10'),
-    to: new Date('2017/12'),
+    from: new Date('2017/10/15'),
+    to: new Date('2017/12/15'),
     profession: 'apprenticeship-tiler',
     company: 'Ulf Martischewski Fliesen, Naturstein, Spanndecken'
   },
   {
-    from: new Date('2016/09'),
-    to: new Date('2017/07'),
+    from: new Date('2016/09/15'),
+    to: new Date('2017/07/15'),
     profession: 'apprenticeship-tiler',
     company: 'WH Werkhaus Berlin'
   },
   {
-    from: new Date('2015/10'),
-    to: new Date('2016/07'),
+    from: new Date('2015/10/15'),
+    to: new Date('2016/07/15'),
     profession: 'apprenticeship-tiler',
     company: 'BfW Lehrbauhof Berlin'
   },
   {
-    from: new Date('2014/12'),
-    to: new Date('2015/01'),
+    from: new Date('2014/12/15'),
+    to: new Date('2015/01/15'),
     profession: 'apprenticeship-it-management-assistant',
     company: 'ACDM (MAIL BOX - Zentrale)'
   },
   {
-    from: new Date('2013/08'),
-    to: new Date('2014/12'),
+    from: new Date('2013/08/15'),
+    to: new Date('2014/12/15'),
     profession: 'apprenticeship-warehouse-logistics-assistant',
     company: 'MAIL BOX - Direct-Marketing Service GmbH'
   },
   {
-    from: new Date('2013/05'),
-    to: new Date('2013/08'),
+    from: new Date('2013/05/15'),
+    to: new Date('2013/08/15'),
     profession: 'part-time-warehouse-logistics-assistant',
     company: 'MAIL BOX - Direct-Marketing Service GmbH'
   },
   {
-    from: new Date('2013/04'),
-    to: new Date('2013/05'),
+    from: new Date('2013/04/15'),
+    to: new Date('2013/05/15'),
     profession: 'internship-warehouse-logistics-assistant',
     company: 'Party Rent Franchise GmbH'
   },
   {
-    from: new Date('2013/01'),
-    to: new Date('2013/04'),
+    from: new Date('2013/01/15'),
+    to: new Date('2013/04/15'),
     profession: 'internship-sport-and-fitness-management-assistant',
     company: 'EasyFitness Prenzlauer Berg'
   },
   {
-    from: new Date('2012/01'),
+    from: new Date('2012/01/15'),
     to: null,
     profession: 'school-internship-repair-service',
     company: 'Phonedoctor Berlin'
