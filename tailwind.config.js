9e4a078307340e739b1629f8746c5955001f5c2e
Sven Brodny <svenbrodny@googlemail.com>
2025-02-19 17:20:05 +0100

    build(tailwind): add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': 'var(--mat-sys-primary)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
