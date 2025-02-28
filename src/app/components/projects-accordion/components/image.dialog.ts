aba1856a03a55be46cbdb691f72a5b11ac2c838e
Sven Brodny <svenbrodny@googlemail.com>
2025-03-01 00:46:07 +0100

    stylefix: image dialog container

--- src/app/components/projects-accordion/components/image.dialog.ts
+++ src/app/components/projects-accordion/components/image.dialog.ts
@@ -8,11 +8,24 @@ export interface DialogData {
 }
 
 @Component({
-    selector: 'sb-image-dialog',
-    imports: [MatDialogModule],
-    template: ` <img [src]="image.src!" [alt]="image.label" mat-dialog-close /> `,
-    styles: `
-    ::ng-deep .project-image-dialog mat-dialog-container { height: 90vh }
+  selector: 'sb-image-dialog',
+  imports: [MatDialogModule],
+  template: ` <img [src]="image.src!" [alt]="image.label" mat-dialog-close /> `,
+  styles: `
+    @use "@angular/material" as mat;
+
+    ::ng-deep .project-image-dialog mat-dialog-container { 
+      max-height: 90vh;
+
+      @include mat.dialog-overrides((
+        container-shape: "0px",
+      ));
+    }
+
+    ::ng-deep .cdk-overlay-pane.mat-mdc-dialog-panel {
+      --mat-dialog-container-max-width: 500px;
+      @screen lg { --mat-dialog-container-max-width: 700px; }
+    }
   `
 })
 export class ImageDialog {
