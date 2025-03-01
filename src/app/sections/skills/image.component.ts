75f48808d978f78ee612df7729cc8a21e96920be
Sven Brodny <svenbrodny@googlemail.com>
2025-03-01 01:29:56 +0100

    feat: defer skills images on viewport

--- src/app/sections/skills/image.component.ts
+++ src/app/sections/skills/image.component.ts
@@ -1,13 +1,27 @@
 import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 import { ImageSource, ImageTitle } from '../../models';
 
 @Component({
-    selector: 'sb-image',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [NgOptimizedImage],
-    template: `<img [ngSrc]="source()" [alt]="title()" [title]="title()" priority width="50" height="50" />`
+  selector: 'sb-image',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgOptimizedImage, MatIconModule],
+  styles: `
+    mat-icon {
+      width: 50px;
+      height: 50px;
+      font-size: 50px;
+    }
+  `,
+  template: `
+    @defer(on viewport) {
+    <img [ngSrc]="source()" [alt]="title()" [title]="title()" priority width="50" height="50" />
+    } @placeholder {
+    <mat-icon>image</mat-icon>
+    }
+  `
 })
 export class ImageComponent {
   source = input.required<ImageSource>();
--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -11,9 +11,39 @@ import { ImageComponent } from './image.component';
 
 @Component({
   selector: 'section#skills',
-  templateUrl: './skills.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule, CardHeaderComponent, ImageComponent, TranslateModule],
+  template: `
+    <div class="card">
+      <sb-card-header title="Skills" [icon]="faBook"></sb-card-header>
+
+      <div class="grid grid-rows-5 grid-flow-col sm:gap-10 gap-4">
+        <div class="text-xs sm:text-base">
+          <p>{{ 'content.skills.very-good-knowledge' | translate }}</p>
+        </div>
+        <sb-image source="assets/skillset/angular.png" title="Angular"></sb-image>
+        <sb-image source="assets/skillset/typescript.png" title="TypeScript"></sb-image>
+        <sb-image source="assets/skillset/html.png" title="HTML5"></sb-image>
+        <sb-image source="assets/skillset/css.png" title="CSS3"></sb-image>
+
+        <div class="text-xs sm:text-base">
+          <p>{{ 'content.skills.advanced-knowledge' | translate }}</p>
+        </div>
+        <sb-image [source]="nodeJsLogo()" title="Node.js"></sb-image>
+        <sb-image source="assets/skillset/javascript.png" title="JavaScript"></sb-image>
+        <sb-image source="assets/skillset/ionic.png" title="Ionic"></sb-image>
+        <sb-image source="assets/skillset/mongodb.png" title="MongoDB"></sb-image>
+
+        <div class="text-xs sm:text-base">
+          <p [innerHTML]="'content.skills.basic-knowledge' | translate"></p>
+        </div>
+        <sb-image source="assets/skillset/react.png" title="React"></sb-image>
+        <sb-image source="assets/skillset/swift.png" title="Swift"></sb-image>
+        <sb-image source="assets/skillset/mysql.png" title="MySQL"></sb-image>
+        <sb-image source="assets/skillset/java.png" title="Java"></sb-image>
+      </div>
+    </div>
+  `,
   styles: `
     @use "src/styles/constants";
 
